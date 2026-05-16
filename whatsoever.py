4ebb7c0beeb5e2d8a7168d275c878e3a6c6e047b
Asanzo San <anushh.verma2403@gmail.com>
2026-05-16 23:39:15 +0530

    C6- just get away

--- whatsoever.py
+++ whatsoever.py
@@ -1,10 +1,8 @@
 # write python function to print the table of given user input
 def table():
-  num = int(input("enter the number : "))
-for i in range (1,11):
-  print(f"{num}*{i}= {num*i}")
+    num = int(input("enter the number : "))
+    for i in range (1,11):
+        print(f"{num}*{i}= {num*i}")
 table()
 
 # rewrite table function using while loop
-def table():
-    num= int
