955e4e83dbed8017cc671a6c22ba2fd812bc22ba
anushkaverma2403-ops <anushh.verma2403@gmail.com>
2026-05-16 18:00:44 +0530

    C4- added table function to whatsoever.py

---
+++ whatsoever.py
@@ -0,0 +1,6 @@
+# write python function to print the table of given user input
+def table():
+  num = int(input("enter the number : "))
+for i in range (1,11):
+  print(f"{num}*{i}= {num*i}")
+table()
